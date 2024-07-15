018326043dc79e99073b363724cf3b8a42d6ab2b
Edivaldo Machado <edivaldomachado@gmail.com>
2024-07-15 12:57:33 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorApp3/wwwroot/Script/JavaScript.js
@@ -0,0 +1,14 @@
+﻿function funcao1() {
+    alert("Eu sou um alert!");
+}
+
+function ReceivingDataChatWoot()
+{
+    window.addEventListener("message", function (event) {
+        if (!isJSONValid(event.data)) {
+            return;
+        }
+
+        const eventData = JSON.parse(event.data);
+    });
+}
